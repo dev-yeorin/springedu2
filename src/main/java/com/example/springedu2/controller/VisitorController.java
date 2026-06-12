126c0adc7ec0bdbc0952373e1723ee7bed884b02
dev-yeorin <par4mit4@gmail.com>
2026-06-12 15:50:22 +0900

    docker compose update

--- src/main/java/com/example/springedu2/controller/VisitorController.java
+++ src/main/java/com/example/springedu2/controller/VisitorController.java
@@ -3,6 +3,7 @@ package com.example.springedu2.controller;
 import com.example.springedu2.entity.Visitor;
 import com.example.springedu2.repositoty.VisitorRepository;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,10 +14,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequiredArgsConstructor
 public class VisitorController {
 
-    @Autowired
+
+    // 1번 방식: @Autowired 대신 생성자 주임
+    // @Autowired
+    // private VisitorRepository visitorRepository;
+
+    // 2번 방식: 생성자 주임: 요즘 방식
+    /*
     private VisitorRepository visitorRepository;
+    public VisitorController(VisitorRepository visitorRepository) {
+        this.visitorRepository = visitorRepository;
+    }*/
+
+    // 3. 생성자 주입  다른 방법
+    // @RequiredArgsConstructor  최상단에 필수 :  Lombok 필수 사용
+    private final VisitorRepository visitorRepository;
 
     @GetMapping("/vlist")
     public ModelAndView vlist(){
@@ -42,7 +57,7 @@ public class VisitorController {
             model.addAttribute("msg","이름과 내용을 모두 입력하세요");
             return "visitorView"; // visitorview.html
         }
-        visitorRepository.save(visitor);; // entity 객체를 사용해야 한다
+        visitorRepository.save(visitor); // entity 객체를 사용해야 한다
 
         return "redirect:/vlist";
     }
